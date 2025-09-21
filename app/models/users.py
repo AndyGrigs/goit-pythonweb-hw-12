65cc60556cd8cf3d419dff57ce16bba348ce3b7a
Andriy <andygrigs88@gmail.com>
2025-09-21 11:20:32 +0200

    make migration db

--- app/models/users.py
+++ app/models/users.py
@@ -2,6 +2,7 @@ from sqlalchemy import Column, Integer, String, Boolean, DateTime, func, Enum
 from sqlalchemy.orm import relationship
 from app.database.base import Base
 import enum
+from datetime import datetime
 
 class UserRole(str, enum.Enum):
     USER ="user"
@@ -15,6 +16,9 @@ class User(Base):
     username = Column(String(50), unique=True, nullable=False, index=True)
     email = Column(String(100), unique=True, nullable=False, index=True)
     hashed_password = Column(String(255), nullable=False)
+    # Поля для скидання пароля
+    reset_password_token = Column(String(255), nullable=True)
+    reset_password_expires = Column(DateTime(timezone=True), nullable=True)
     avatar_url = Column(String(255), nullable=True)
     is_verified = Column(Boolean, default=False)
     role = Column(Enum(UserRole), nullable=True)
